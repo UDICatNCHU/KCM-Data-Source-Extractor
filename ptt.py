58053c62600191ddc7c9a89ba28fc4014f2df114
davidtnfsh <davidtnfsh@gmail.com>
2017-02-20 08:53:41 +0800

    add sys.argv[0]

--- ptt.py
+++ ptt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import json
+import json, sys
 with open('ptt.kcm', 'w', encoding='utf-8') as f:
-	for i in map(lambda x:x['content'], json.load(open('ptt.json', 'r'))['articles']):
+	for i in map(lambda x:x['content'], json.load(open(sys.argv[1], 'r'))['articles']):
 		f.write(i + '\n')
