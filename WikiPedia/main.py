bab91c63456128302a0409650ccc759ef2672833
davidtnfsh <davidtnfsh@gmail.com>
2017-02-28 21:13:23 +0800

    finish wikipart, append all text file into one

--- WikiPedia/main.py
+++ WikiPedia/main.py
@@ -5,7 +5,8 @@
 This script is used to generate KCM/TCM (Term correlated model).
 The task is done by calling many other scripts
 """
-import os, logging
+import os, subprocess, sys, logging
+from pathlib import Path
 class KCM(object):
     """docstring for KCM"""
     def __init__(self):
@@ -18,6 +19,19 @@ class KCM(object):
         subprocess.call(['bash', os.path.join('langConfig', lang+'.sh'), os.getcwd()])
         print(os.getcwd())
 
+def hasFile():
+        file = Path('wiki.txt.all')
+        if file.is_file():
+            return True
+        else: return False
+
+
 if __name__ == '__main__':
-    k = KCM()
-    k.download('cht', 'WikiRaw/')
+    if hasFile():
+        os.remove('wiki.txt.all')
+    with open('wiki.txt.all', "a") as outfile:
+        for (dir_path, dir_names, file_names) in os.walk('WikiRaw/cht'):
+            print(dir_path, dir_names, file_names)
+            if file_names != []:
+                for f in file_names:
+                    subprocess.call(['cat', os.path.join(dir_path, f)], stdout=outfile)
